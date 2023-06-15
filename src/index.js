e47749451d756c0e7f1fb87ae3bc4ebeac599a67
MaxitoMix <128189550+MaxitoMix@users.noreply.github.com>
2023-06-15 14:45:28 -0400

    .

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import './style.css';
 
 //---variables
 const container = document.getElementsByClassName('container')[0]
+const info = document.getElementsByClassName('info')[0]
+const roster = document.getElementsByClassName('roster')[0];
+const controls = document.getElementsByClassName('controls')[0];
 let rosterAngle = '0deg'
+let currentPlayer 
 
 
 
@@ -12,32 +16,50 @@ let rosterAngle = '0deg'
 //-------------------
 
 
-//-------------------
+//-------roster area
 
+function displayRoster() {
+    const rosterArea = document.createElement('div');
+    rosterArea.className = 'roster-area';
 
-const roster = document.getElementsByClassName('roster-area')[0];
-const rosterShips = Array.from(roster.children)
+    const shipData = [
+        {name: 'destroyer', size: 3},
+        {name: 'battleship', size: 4},
+        {name: 'submarine', size: 3},
+        {name: 'carrier', size: 5},
+        {name: 'tug', size: 2}
+    ];
 
+    shipData.forEach(ship => {
+        const shipDiv = document.createElement('div');
+        shipDiv.className = `${ship.name}-roster ${ship.name}`;
+        shipDiv.draggable = 'true';
+        shipDiv.dataset.name = ship.name;
+        shipDiv.dataset.size = ship.size;
+        
+        rosterArea.appendChild(shipDiv);
+    });
 
-const rotateBtn = document.getElementById('rotate');
-rotateBtn.addEventListener('click', rotate)
+    // Append the rosterArea to your container (or another element in your DOM)
+    
+    roster.appendChild(rosterArea);
 
-function rotate(){
-    rosterAngle ===  '0deg'? rosterAngle = '90deg': rosterAngle = '0deg'
+    const rosterShips = Array.from(rosterArea.children)
+
     rosterShips.forEach(rostership => {
-        rostership.style.transform = `rotate(${rosterAngle})`;
-        rostership.dataset.rosterAngle = rosterAngle;
+        rostership.addEventListener('dragstart', dragShipStart)
     })
     
+    rosterShips.forEach(rostership => {
+        rostership.addEventListener('dragend',dragShipEnd)
+    })
+
+    return 
 }
 
-rosterShips.forEach(rostership => {
-    rostership.addEventListener('dragstart', dragShipStart)
-})
 
-rosterShips.forEach(rostership => {
-    rostership.addEventListener('dragend',dragShipEnd)
-})
+
+
 
 
 function dragShipStart(e){
@@ -47,19 +69,67 @@ function dragShipStart(e){
     console.log('text', dataSize +','+ dataName)
     e.dataTransfer.setData('text', dataSize +','+ dataName);
 
-  }
+}
 
 function dragShipEnd(){
     console.log('drag-end')
 }
 
+//-----------end roster area------------------------------------------------------------
+//----controls area----------
 
+function displayControls(){
+    // Create the rotate button
+    const rotateBtn = document.createElement('button');
+    rotateBtn.id = 'rotate';
+    rotateBtn.textContent = 'Rotate';
 
-function Gameboard(){
-    let board = Array(10).fill(null).map(() => Array(10).fill(null))
+    // Create the reset button
+    const resetBtn = document.createElement('button');
+    resetBtn.id = 'reset';
+    resetBtn.textContent = 'Reset Game';
 
+    // Append the buttons to the parent element
+    controls.appendChild(rotateBtn);
+    controls.appendChild(resetBtn);
+
+    
+    
+    
+    
+    // const resetBtn = document.getElementById('reset');
+    resetBtn.addEventListener('click', reset)
+    
+   
+    // const rotateBtn = document.getElementById('rotate');
+    rotateBtn.addEventListener('click', rotate)
+    
+    
+}
+
+function reset(){
+    rosterAngle =  '0deg';
+    container.innerHTML = '';
+    game()
+}
+
+function rotate(){
+    rosterAngle ===  '0deg'? rosterAngle = '90deg': rosterAngle = '0deg'
+    rosterShips.forEach(rostership => {
+        rostership.style.transform = `rotate(${rosterAngle})`;
+        rostership.dataset.rosterAngle = rosterAngle;
+    })
+}
+
+//----controls area end----------
+
+function Gameboard(name){
+    let board = Array(10).fill(null).map(() => Array(10).fill(null));
+    
     function placeShip(dataCoord, dataDropped){ 
-        let grid = document.getElementsByClassName('grid')[0];
+        // let grid = document.getElementsByClassName('grid')[0];
+        // let grid = currentPlayer.board
+        let grid = document.querySelector(`.grid[data-player="${currentPlayer.name}"]`);
         // console.log(grid)
         let  [col,row] = dataCoord.split(',')
         let [size,name] = dataDropped.split(',')
@@ -95,27 +165,33 @@ function Gameboard(){
 
                 cell.dataset.content = name;
                 cell.classList.add(name)
-                let rosterItem = roster.getElementsByClassName(`${name}-roster`)[0];
-                rosterItem.remove()
+
             }
+
+            const rosterArea = document.getElementsByClassName('roster-area')[0];
+            let rosterItem = rosterArea.getElementsByClassName(`${name}-roster`)[0];
+            rosterItem.remove()
             
         }else {
+            info.textContent = 'unable to place ship. try somewhere else.'
             console.log('unable to place ship. try somewhere else.')    
+        } 
 
-    } 
 
     }
 
    
     return{
         board,
-        placeShip
+        placeShip,
+        name
     }
 }
 
 
 function checkOutOfBounds(col, row, size) {
-    let grid = document.getElementsByClassName('grid')[0];
+    // let grid = document.getElementsByClassName('grid')[0];
+    let grid = document.querySelector(`.grid[data-player="${currentPlayer.name}"]`);
     let numCol = Number(col);
     let numRow = Number(row);
     let stringCol, stringRow;
@@ -149,8 +225,9 @@ function displayBoard(board){
     let content = document.getElementsByClassName('container')[0];
     let grid = document.createElement('div');
     grid.classList.add('grid');
-    content.appendChild(grid)
-    
+    grid.dataset.player = currentPlayer.name;
+    content.appendChild(grid);
+
     for (let col = 0; col < board.length; col++) {
         for (let row = 0; row < board[col].length; row++) {
 
@@ -197,7 +274,7 @@ function handleDrop(e){
     console.log(dataCoord);
     let dataDropped = e.dataTransfer.getData("text");
     console.log('Dropped data: ', dataDropped);
-    player1.placeShip(dataCoord,dataDropped )
+    currentPlayer.placeShip(dataCoord,dataDropped )
 
 }
 
@@ -219,19 +296,28 @@ function Ship(name, length){
     }
 }
 
-//---run code 
-let destroyerP1 = new Ship('destroyerP1', 3)
+function game(){
+    //---run code 
+    // let destroyerP1 = new Ship('destroyerP1', 3)
 
-console.log(destroyerP1)
+    // console.log(destroyerP1)
 
-let player1 = new Gameboard()
-let player2 = new Gameboard()
+    let player1 = new Gameboard('player1')
+    let player2 = new Gameboard('player2')
+
+    currentPlayer = player1
 
 
-console.log(player1)
-console.log(player2)
+    console.log(player1)
+    // console.log(player2)
 
-displayBoard(player1.board)
-displayBoard(player2.board)
+    displayBoard(player1.board)
+    displayRoster();
+    displayControls();
+    // currentPlayer = 'player2'
+    // displayBoard(player2.board)
 
-console.log(destroyerP1.name)
+    // console.log(destroyerP1.name)
+}
+
+game()
