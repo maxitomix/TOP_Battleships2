3adf3762145294ec19dba098df157caf7a317c3c
MaxitoMix <128189550+MaxitoMix@users.noreply.github.com>
2023-06-16 14:42:05 -0400

    advance

--- src/index.js
+++ src/index.js
@@ -37,6 +37,7 @@ function displayRoster() {
         shipDiv.draggable = 'true';
         shipDiv.dataset.name = ship.name;
         shipDiv.dataset.size = ship.size;
+        shipDiv.dataset.rosterAngle = '0deg'
         
         rosterArea.appendChild(shipDiv);
     });
@@ -122,13 +123,15 @@ function Gameboard(name){
     let ships = [];
 
     function placeShip(dataCoord, dataDropped){ 
-
+        console.log('dataCoord',dataCoord)
+        console.log('dataDropped',dataDropped)
         let grid = document.querySelector(`.grid[data-player="${currentPlayer.name}"]`);
+        console.log('grid:',grid)
         let  [col,row] = dataCoord.split(',')
         let [size,name] = dataDropped.split(',')
         console.log(col, row)
         let cell = grid.querySelector(`.cell[data-coord="${col},${row}"]`)
-        console.log(cell)
+        console.log('cell:',cell)
         console.log(name,size)
 
         
@@ -152,7 +155,7 @@ function Gameboard(name){
                 if (rosterAngle === '90deg'){
                    
                     console.log(`.cell[data-coord="${col},${stringRow}"]`)
-                    cell = grid.querySelector(`.cell[data-coord="${col}, ${stringRow}"]`)
+                    cell = grid.querySelector(`.cell[data-coord="${col},${stringRow}"]`)
                     console.log(cell)
                 }
 
@@ -160,12 +163,16 @@ function Gameboard(name){
                 cell.classList.add(name);
             }
 
+
+            let newShip = new Ship(name, size);
+            ships.push(newShip);
+            console.log(currentPlayer.name,ships)
+
+            if (currentPlayer === player1){
             const rosterArea = document.getElementsByClassName('roster-area')[0];
             let rosterItem = rosterArea.getElementsByClassName(`${name}-roster`)[0];
             rosterItem.remove()
-            let newShip = new Ship(name, size);
-            ships.push(newShip);
-            console.log(ships)
+            }
             
         }else {
             info.textContent = 'unable to place ship. try somewhere else.'
@@ -188,25 +195,35 @@ function Gameboard(name){
 
 function checkOutOfBounds(col, row, size) {
     let grid = document.querySelector(`.grid[data-player="${currentPlayer.name}"]`);
+    console.log('checkOutOfBounds grid:',`.grid[data-player="${currentPlayer.name}"]`)
+    console.log(grid)
     let numCol = Number(col);
     let numRow = Number(row);
     let stringCol, stringRow;
 
     for (let i = 0; i < size; i++) {
         if (rosterAngle === '0deg') {
+            console.log('start debug 0deg',row)
+            console.log(numCol)
             stringCol = (numCol + i).toString();
+            console.log(stringCol)
+            console.log(`.cell[data-coord="${stringCol},${row}"]`)
             let cell = grid.querySelector(`.cell[data-coord="${stringCol},${row}"]`);
-            if (!cell || cell.dataset.content !== 'water') return false;
+            console.log(cell)
+            if (!cell || cell.dataset.content !== 'water') 
+            return 
+                false, console.log('fail 0deg')
+            
         }
         if (rosterAngle === '90deg') {
-            console.log('start debug',col)
+            console.log('start debug 90deg',col)
             console.log(numRow)
             stringRow = (numRow + i).toString();
             console.log(stringRow)
-            console.log(`.cell[data-coord="${col}, ${stringRow}"]`)
-            let cell = grid.querySelector(`.cell[data-coord="${col}, ${stringRow}"]`);
+            console.log(`.cell[data-coord="${col},${stringRow}"]`)
+            let cell = grid.querySelector(`.cell[data-coord="${col},${stringRow}"]`);
             console.log(cell)
-            if (!cell || cell.dataset.content !== 'water') return false;
+            if (!cell || cell.dataset.content !== 'water') return false, console.log('fail 90deg')
         }
     }
     return true;
@@ -228,7 +245,7 @@ function displayBoard(board){
         for (let row = 0; row < board[col].length; row++) {
 
             let cell = document.createElement('div');
-            cell.dataset.coord = `${row}, ${col}`
+            cell.dataset.coord = `${row},${col}`
             cell.dataset.content = 'water'
 
             cell.addEventListener('click', () =>{
@@ -272,8 +289,8 @@ function cellClickLogic(cell){
         if (ship.isSunk()){
             cell.dataset.content = 'hit'
             cell.classList.add('hit') 
-            info.textContent = `Its a HIT! You sank a${ship.name}`
-            console.log(`Its a HIT! You sank a${ship.name}`)  
+            info.textContent = `Its a HIT! You sank a ${ship.name}`
+            console.log(`Its a HIT! You sank a ${ship.name}`)  
             blinkElement(info)
             currentPlayer.ships = currentPlayer.ships.filter(x => x.name !== ship.name)
             console.log(currentPlayer.ships)
@@ -355,6 +372,47 @@ function Ship(name, length){
 }
 
 //-----------------ship end
+//place ship random
+
+function placeShipsRandom(currentPlayer) {
+    const shipData = [
+        {name: 'destroyer', size: 3},
+        {name: 'battleship', size: 4},
+        {name: 'submarine', size: 3},
+        {name: 'carrier', size: 5},
+        {name: 'tug', size: 2}
+    ];
+
+    shipData.forEach(ship =>{
+        let placed = false;
+        while (!placed){
+            let row = Math.floor(Math.random() * 10);
+            let col = Math.floor(Math.random() * 10);
+            let dataCoord = `${col},${row}`;
+            console.log(dataCoord)
+            let dataDropped = `${ship.size},${ship.name}`;
+            console.log(dataDropped);
+
+            rosterAngle = Math.random() > 0.5 ? '0deg' : '90deg';
+            // rosterAngle = '0deg'
+            
+            console.log(rosterAngle);
+
+            if (checkOutOfBounds(col, row, ship.size)){
+                console.log('placed')
+                currentPlayer.placeShip(dataCoord, dataDropped)
+                placed = true
+                
+            }else{
+                console.log('NotPlaced')
+            }
+        }
+    })
+
+    rosterAngle = '0deg'
+}
+
+
 
 function game(){
 
@@ -366,13 +424,25 @@ function game(){
     currentPlayer = player1
 
 
-    console.log(player1)
+    console.log(currentPlayer.name)
 
 
     displayBoard(player1.board)
+    currentPlayer = player2
+    console.log(currentPlayer.name)
+    displayBoard(player2.board)
+
+
     displayRoster();
     displayControls();
 
+    placeShipsRandom(currentPlayer)
+   
+    
+    console.log(currentPlayer.name)
+    currentPlayer = player1
+    console.log(currentPlayer.name)
+    
 }
 
 game()
